f11ae04b94c80477a8af6f8de05172f655188bfd
QuangPoppy163 <QuangBobby163@gmail.com>
2018-11-06 17:22:56 +0700

    Bai nhap mon

---
+++ MyNetCoreMVC/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function() {
+    $(".btnDeleteClick").click(function() {
+        const currentBtn = $(this);
+        console.log(currentBtn.attr("id"));
+        if (confirm("Do you delete?")) {
+            $.ajax({
+                url: `Delete?id=${currentBtn.attr("id")}`,
+                method: "DELETE",
+                success: function(data) {
+                    console.log("Success Delete");
+                    currentBtn.parent().parent().remove();
+                }
+        });
+        }
+    });
+})
